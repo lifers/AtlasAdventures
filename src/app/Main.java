0e486405b5f4788b1ed2602a0569787a05da2ebf
Yoshiku Yuu <119818606+YoshikuYuu@users.noreply.github.com>
2023-10-23 20:37:16 -0400

    Starting Template File Structure

---
+++ src/app/Main.java
@@ -0,0 +1,24 @@
+package app;
+
+import data_access.FileUserDataAccessObject;
+import entity.CommonUserFactory;
+import interface_adapter.clear_users.ClearViewModel;
+import interface_adapter.login.LoginViewModel;
+import interface_adapter.logged_in.LoggedInViewModel;
+import interface_adapter.signup.SignupViewModel;
+import interface_adapter.ViewManagerModel;
+import use_case.login.LoginUserDataAccessInterface;
+import view.LoggedInView;
+import view.LoginView;
+import view.SignupView;
+import view.ViewManager;
+
+import javax.swing.*;
+import java.awt.*;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
