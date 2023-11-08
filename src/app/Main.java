edd0a40ada8c5f38a97f8adb4b366944d5ef8a73
Yoshiku Yuu <119818606+YoshikuYuu@users.noreply.github.com>
2023-11-08 16:08:35 -0500

    Add skeleton for Main.java

--- src/app/Main.java
+++ src/app/Main.java
@@ -19,6 +19,43 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
+        // Build the main program window, the main panel containing the
+        // various cards, and the layout, and stitch them together.
 
+        // The main application window.
+        JFrame application = new JFrame("AtlasAdventures");
+        application.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        CardLayout cardLayout = new CardLayout();
+
+        // The various View objects. Only one view is visible at a time.
+        JPanel views = new JPanel(cardLayout);
+        application.add(views);
+
+        // This keeps track of and manages which view is currently showing.
+        ViewManagerModel viewManagerModel = new ViewManagerModel();
+        new ViewManager(views, cardLayout, viewManagerModel);
+
+        // The data for the views, such as username and password, are in the ViewModels.
+        // This information will be changed by a presenter object that is reporting the
+        // results from the use case. The ViewModels are observable, and will
+        // be observed by the Views.
+        StartQuizViewModel startQuizViewModel = new StartQuizViewModel();
+        QuizViewModel quizViewModel = new QuizViewModel();
+
+        // Dummy data access object, to be replaced with actual DAO
+        DummyDAO dummyDAO = new DummyDAO();
+
+        StartQuizView startQuizView = StartQuizUseCaseFactory.create(viewManagerModel, startQuizViewModel, quizViewModel, dummyDAO);
+        views.add(startQuizView, startQuizView.viewName);
+
+        QuizView quizView = new quizView();
+        views.add(quizView, quizView.viewName);
+
+        viewManagerModel.setActiveView(startQuizView.viewName);
+        viewManagerModel.firePropertyChanged();
+
+        application.pack();
+        application.setVisible(true);
     }
 }
