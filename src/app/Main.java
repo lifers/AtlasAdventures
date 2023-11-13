93464afe7a5d4c7ec72b2c3a3677a41ae61d45f3
Yoshiku Yuu <119818606+YoshikuYuu@users.noreply.github.com>
2023-11-13 14:40:32 -0500

    Add SPQuiz elements into Main, create skeleton SPQuiz interactor Add SPQuiz view, view model, and use case factory into Main.java. Create skeleton SPQuiz interactor for testing.

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,16 +1,17 @@
 package app;
 
 import com.formdev.flatlaf.FlatDarkLaf;
+import data_access.DummyDAO;
 import data_access.MapDataAccessObject;
 import interface_adapter.ViewManagerModel;
-import interface_adapter.start_quiz.StartQuizViewModel;
+import interface_adapter.start_sp_quiz.SPQuizViewModel;
 import interface_adapter.question.QuestionViewModel;
+import view.MainMenuView;
 import view.QuestionView;
 import view.ViewManager;
 
 import javax.swing.*;
 import java.awt.*;
-import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
@@ -38,29 +39,20 @@ public class Main {
         // results from the use case. The ViewModels are observable, and will
         // be observed by the Views.
 
-        //StartQuiz Use Case
-        StartQuizViewModel startQuizViewModel = new StartQuizViewModel();
-        QuizViewModel quizViewModel = new QuizViewModel();
+        SPQuizViewModel spQuizViewModel = new SPQuizViewModel();
+        QuestionViewModel questionViewModel = new QuestionViewModel();
 
+        MapDataAccessObject mapDataAccessObject = new MapDataAccessObject();
         // Dummy data access object, to be replaced with actual DAO
         DummyDAO dummyDAO = new DummyDAO();
 
-        StartQuizView startQuizView = StartQuizUseCaseFactory.create(viewManagerModel, startQuizViewModel, quizViewModel, dummyDAO);
-        views.add(startQuizView, startQuizView.viewName);
-
-        QuizView quizView = new quizView();
-        views.add(quizView, quizView.viewName);
-
-        viewManagerModel.setActiveView(startQuizView.viewName);
-        viewManagerModel.firePropertyChanged();
-
-        //Question Use Case
-        QuestionViewModel questionViewModel = new QuestionViewModel();
-        MapDataAccessObject mapDataAccessObject = new MapDataAccessObject();
         QuestionView questionView = QuestionUseCaseFactory.create(viewManagerModel, questionViewModel, mapDataAccessObject);
         views.add(questionView);
 
-        viewManagerModel.setActiveView(questionView);
+        MainMenuView mainMenuView = SPUseCaseFactory.create(viewManagerModel, spQuizViewModel, questionViewModel, dummyDAO);
+        views.add(mainMenuView);
+
+        viewManagerModel.setActiveView(mainMenuView);
         viewManagerModel.firePropertyChanged();
 
         application.pack();
