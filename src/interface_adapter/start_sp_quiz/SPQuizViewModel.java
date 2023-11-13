45e24c6337184ffaedb31e011a40525bab7e390f
Yoshiku Yuu <119818606+YoshikuYuu@users.noreply.github.com>
2023-11-13 16:08:25 -0500

    Modify ViewModel viewNames so that ViewManager works Give QuestionViewModel the same viewName as QuestionView, and SPQuizViewModel the same viewName as MainMenuView

--- src/interface_adapter/start_sp_quiz/SPQuizViewModel.java
+++ src/interface_adapter/start_sp_quiz/SPQuizViewModel.java
@@ -10,7 +10,7 @@ public class SPQuizViewModel extends ViewModel {
     public static final String SPQUIZ_BUTTON_LABEL = "Single Player";
 
     public SPQuizViewModel() {
-        super("StartSPQuiz");
+        super("MainMenuView");
     }
 
     private final PropertyChangeSupport support = new PropertyChangeSupport(this);
