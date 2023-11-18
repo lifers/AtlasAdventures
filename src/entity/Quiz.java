601f7bca2db7de67f23f62629a4a8c858fbd319c
Nagata Parama Aptana <61454123+lifers@users.noreply.github.com>
2023-11-18 15:35:34 -0500

    require calling nextQuestion() to start the quiz

--- src/entity/Quiz.java
+++ src/entity/Quiz.java
@@ -5,7 +5,7 @@ import java.util.List;
 public class Quiz {
     private final List<Question> questions;
     private final int length;
-    private int currQuestionIndex = 0;
+    private int currQuestionIndex = -1;
 
     public Quiz(List<Question> questions) {
         this.questions = questions;
