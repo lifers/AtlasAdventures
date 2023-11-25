03c6b97aa2078f5dcc7d2ba781503cefbf181148
Yoshiku Yuu <119818606+YoshikuYuu@users.noreply.github.com>
2023-11-24 19:01:05 -0500

    Import java.util.*

--- src/app/Main.java
+++ src/app/Main.java
@@ -19,7 +19,9 @@ import use_case.profile.ProfileDataAccessInterface;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.*;
 import java.io.IOException;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
