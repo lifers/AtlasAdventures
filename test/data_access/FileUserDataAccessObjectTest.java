e66a1d7bc99117d509cd3516383fa4d68dbf75d3
Connor Sicheri <connorsicheri@Administrators-MacBook-Pro.local>
2023-11-30 21:52:53 -0500

    finalized tests and minor midification to DAO

--- test/data_access/FileUserDataAccessObjectTest.java
+++ test/data_access/FileUserDataAccessObjectTest.java
@@ -15,21 +15,21 @@ class FileUserDataAccessObjectTest {
     void update() throws IOException {
         // Create a test instance of FileUserDataAccessObject
         try{
-            FileUserDataAccessObject userDataAccessObject = new FileUserDataAccessObject("test.csv");
+            FileUserDataAccessObject dataAccessObject = new FileUserDataAccessObject("test.csv");
             // Set up the initial state of the test data
-            userDataAccessObject.setAverageScore(3.5);
-            userDataAccessObject.setGamesPlayed(10);
+            dataAccessObject.setAverageScore(3.5);
+            dataAccessObject.setGamesPlayed(10);
 
             // Call the update method
             try {
-                userDataAccessObject.update();
+                dataAccessObject.update();
             } catch (IOException e) {
                 fail("IOException occurred during update");
             }
 
             // Verify that the data has been updated correctly
-            assertEquals(7.2, userDataAccessObject.getAverageScore(), 0.01);
-            assertEquals(15, userDataAccessObject.getGamesPlayed());
+            assertEquals(7.2, dataAccessObject.getAverageScore(), 0.01);
+            assertEquals(15, dataAccessObject.getGamesPlayed());
         } catch (IOException e){
             fail("Initializer error in FileUserDataAccessObject");
         }
@@ -42,12 +42,12 @@ class FileUserDataAccessObjectTest {
     @Test
     void setGamesPlayed() {
         try{
-            FileUserDataAccessObject userDataAccessObject = new FileUserDataAccessObject("test.csv");
+            FileUserDataAccessObject dataAccessObject = new FileUserDataAccessObject("test.csv");
 
             // Set up the initial state of the test data
-            userDataAccessObject.setGamesPlayed(10);
+            dataAccessObject.setGamesPlayed(10);
 
-            assertEquals(10, userDataAccessObject.getGamesPlayed());
+            assertEquals(10, dataAccessObject.getGamesPlayed());
         } catch (IOException e){
             fail("Initializer error in FileUserDataAccessObject");
         }
@@ -59,12 +59,12 @@ class FileUserDataAccessObjectTest {
     @Test
     void setAverageScore() {
         try{
-            FileUserDataAccessObject userDataAccessObject = new FileUserDataAccessObject("test.csv");
+            FileUserDataAccessObject dataAccessObject = new FileUserDataAccessObject("test.csv");
 
             // Set up the initial state of the test data
-            userDataAccessObject.setAverageScore(3.5);
+            dataAccessObject.setAverageScore(3.5);
 
-            assertEquals(3.5, userDataAccessObject.getAverageScore());
+            assertEquals(3.5, dataAccessObject.getAverageScore());
         } catch (IOException e){
             fail("Initializer error in FileUserDataAccessObject");
         }
@@ -77,9 +77,9 @@ class FileUserDataAccessObjectTest {
     void getGamesPlayed() {
         // Create a test instance of FileUserDataAccessObject
         try{
-            FileUserDataAccessObject userDataAccessObject = new FileUserDataAccessObject("test.csv");
+            FileUserDataAccessObject dataAccessObject = new FileUserDataAccessObject("test.csv");
 
-            assertEquals(15, userDataAccessObject.getGamesPlayed());
+            assertEquals(15, dataAccessObject.getGamesPlayed());
         } catch (IOException e){
             fail("Initializer error in FileUserDataAccessObject");
         }
@@ -92,11 +92,27 @@ class FileUserDataAccessObjectTest {
     void getAverageScore() {
         // Create a test instance of FileUserDataAccessObject
         try{
-            FileUserDataAccessObject userDataAccessObject = new FileUserDataAccessObject("test.csv");
+            FileUserDataAccessObject dataAccessObject = new FileUserDataAccessObject("test.csv");
 
-            assertEquals(7.2, userDataAccessObject.getAverageScore());
+            assertEquals(7.2, dataAccessObject.getAverageScore());
         } catch (IOException e){
             fail("Initializer error in FileUserDataAccessObject");
+        }
+    }
+
+    @Test
+    void save() {
+        try {
+            FileUserDataAccessObject dataAccessObject = new FileUserDataAccessObject("test2.csv");
+            dataAccessObject.setAverageScore(3.5);
+            dataAccessObject.setGamesPlayed(10);
+            dataAccessObject.save();
+            FileUserDataAccessObject dao2 = new FileUserDataAccessObject("test2.csv");
+            assertEquals(dao2.getAverageScore(), 3.5);
+            assertEquals(dao2.getGamesPlayed(), 10);
+        }
+        catch (IOException e){
+            fail("Save/Initializer error in FileUserDataAccessObject");
         }
     }
 }
