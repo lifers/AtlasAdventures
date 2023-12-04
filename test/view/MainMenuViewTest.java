fd15d0809627dc194423197165ca707bb415ac66
Nagata Parama Aptana <61454123+lifers@users.noreply.github.com>
2023-12-04 14:29:16 -0500

    add MainMenuViewTest

--- test/view/MainMenuViewTest.java
+++ test/view/MainMenuViewTest.java
@@ -1,24 +1,12 @@
 package view;
 
 import app.Main;
-import app.MainMenuFactory;
-import interface_adapter.ViewManagerModel;
-import interface_adapter.answer_question.AnswerQuestionViewModel;
-import interface_adapter.answer_question.QuizEndedViewModel;
-import interface_adapter.leaderboard.LeaderboardViewModel;
-import interface_adapter.profile.ProfileViewModel;
-import interface_adapter.start_sp_quiz.SPQuizViewModel;
-import org.junit.Test;
-import use_case.leaderboard.LeaderboardDataAccessInterface;
-import use_case.profile.ProfileDataAccessInterface;
-import use_case.start_sp_quiz.SPQuizDataAccessInterface;
+import org.junit.jupiter.api.Test;
 
 import javax.swing.*;
 import java.awt.*;
-import java.util.Arrays;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class MainMenuViewTest {
 
@@ -54,11 +42,11 @@ public class MainMenuViewTest {
     public void allButtonsPresent() {
         Main.main(null);
         JButton spQuizButton = getButton("SPQuiz");
-        assert(spQuizButton.getText().equals("Single Player"));
+        assert (spQuizButton.getText().equals("Single Player"));
         JButton profileButton = getButton("Profile");
-        assert(profileButton.getText().equals("Local Profile Statistics"));
+        assert (profileButton.getText().equals("Local Profile Statistics"));
         JButton leaderboardButton = getButton("Leaderboard");
-        assert(leaderboardButton.getText().equals("Leaderboard"));
+        assert (leaderboardButton.getText().equals("Leaderboard"));
     }
 
     @Test
@@ -81,37 +69,4 @@ public class MainMenuViewTest {
         JButton leaderboardButton = getButton("Leaderboard");
         leaderboardButton.doClick();
     }
-
-
-    // Instantiate all the view models and DAOs
-//    ViewManagerModel viewManagerModel = new ViewManagerModel();
-//    SPQuizViewModel spQuizViewModel = new SPQuizViewModel();
-//    AnswerQuestionViewModel answerQuestionViewModel = new AnswerQuestionViewModel();
-//    ProfileViewModel profileViewModel = new ProfileViewModel();
-//    LeaderboardViewModel leaderboardViewModel = new LeaderboardViewModel();
-//
-//    SPQuizDataAccessInterface spQuizDAO = null;
-//    ProfileDataAccessInterface profileDAO = null;
-//    LeaderboardDataAccessInterface leaderboardDAO = null;
-//
-//    MainMenuView mainMenuView = MainMenuFactory.create(
-//            viewManagerModel,
-//            spQuizViewModel,
-//            spQuizDAO,
-//            answerQuestionViewModel,
-//            profileViewModel,
-//            profileDAO,
-//            leaderboardViewModel,
-//            leaderboardDAO);
-//
-//    JLabel title = (JLabel) mainMenuView.getComponent(0);
-//    assertEquals(title.getText(), "Main Menu");
-//
-//    JPanel buttons = (JPanel) mainMenuView.getComponent(1);
-//    JButton spButton = (JButton) buttons.getComponent(0);
-//    JButton profileButton = (JButton) buttons.getComponent(1);
-//    JButton leaderboardButton = (JButton) buttons.getComponent(2);
-//    assertEquals(spButton.getText(), "Single Player");
-//    assertEquals(profileButton.getText(), "Local Profile Statistics");
-//    assertEquals(leaderboardButton.getText(), "Leaderboard");
 }
